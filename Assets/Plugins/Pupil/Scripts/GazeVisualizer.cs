e11aad510f8cc01fb46e38e3c6739337133ef356
Gabriel Diaz <performlabrit@users.noreply.github.com>
2019-03-27 10:54:22 -0400

    First commit

---
+++ Assets/Plugins/Pupil/Scripts/GazeVisualizer.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+    public class GazeVisualizer : MonoBehaviour
+    {
+        public SubscriptionsController subscriptionsController;
+        public CalibrationController calibrationController;
+        public Transform gazeEstimateMarker;
+        public Transform cameraTransform;
+
+        [Header("Confidence")]
+        public bool filterByConfidence = true;
+        [Range(0f, 1f)]
+        public float confidenceThreshold = 0.6f;
+
+        [Header("Fixed Depth")]
+        public bool applyFixedDepth = false;
+        [Range(0f, 10f)]
+        public float fixedDepth = 2f;
+
+        [Header("Projected Visualization")]
+        public bool showProjectedVis = true;
+        public Transform projectionMarker;
+        [Range(0.01f, 0.1f)]
+        public float sphereCastRadius = 0.05f;
+
+        GazeListener gazeListener = null;
+        Vector3 localGazePosition;
+        Vector3 worldGazePosition;
+
+        void OnEnable()
+        {
+            if (gazeListener == null)
+            {
+                gazeListener = new GazeListener(subscriptionsController);
+            }
+
+            if (calibrationController == null)
+            {
+                Debug.LogWarning("CalibrationController missing");
+                return;
+            }
+
+            if (cameraTransform == null)
+            {
+                Debug.LogWarning("Camera reference missing");
+                return;
+            }
+
+            calibrationController.OnCalibrationSucceeded += StartVisualizing;
+        }
+
+        void OnDisable()
+        {
+            StopVisualizing();
+        }
+
+        void Update()
+        {
+            gazeEstimateMarker.position = worldGazePosition;
+
+            if (applyFixedDepth || showProjectedVis)
+            {
+                gazeEstimateMarker.position = GetAsFixedDepth(localGazePosition);
+            }
+
+            if (showProjectedVis)
+            {
+                gazeEstimateMarker.gameObject.SetActive(false);
+                ShowProjectedVis();
+            }
+            else
+            {
+                gazeEstimateMarker.gameObject.SetActive(true);
+                if (projectionMarker != null)
+                {
+                    projectionMarker.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        void StartVisualizing()
+        {
+            Debug.Log("Start Visualizing Gaze");
+            gazeListener.OnReceive3dGaze += ReceiveGaze;
+
+            if (gazeEstimateMarker == null)
+            {
+                Debug.LogWarning("Gaze Marker missing");
+                return;
+            }
+
+            gazeEstimateMarker.gameObject.SetActive(true);
+        }
+
+        void StopVisualizing()
+        {
+
+            if (gazeEstimateMarker != null)
+            {
+                gazeEstimateMarker.gameObject.SetActive(false);
+            }
+        }
+
+        void ReceiveGaze(GazeData gazeData)
+        {
+            if (filterByConfidence && gazeData.Confidence >= confidenceThreshold)
+            {
+                localGazePosition = gazeData.GazePoint3d;
+                worldGazePosition = cameraTransform.localToWorldMatrix.MultiplyPoint(gazeData.GazePoint3d);
+            }
+        }
+
+        private Vector3 GetAsFixedDepth(Vector3 localPosition)
+        {
+            localPosition.Normalize();
+
+            //in case depth is < 0
+            float angle = Vector3.Angle(Vector3.forward, localPosition);
+            float direction = 1f;
+            if (angle >= 90f)
+            {
+                direction = -1f;
+            }
+
+            localPosition *= direction * fixedDepth;
+            return cameraTransform.localToWorldMatrix.MultiplyPoint(localPosition);
+        }
+
+        private void ShowProjectedVis()
+        {
+            if (projectionMarker == null)
+            {
+                Debug.LogWarning("Marker missing");
+                return;
+            }
+
+            projectionMarker.gameObject.SetActive(true);
+
+            Vector3 origin = cameraTransform.position;
+
+            Vector3 direction = worldGazePosition - origin;
+            direction.Normalize();
+            if (Physics.SphereCast(origin, sphereCastRadius, direction, out RaycastHit hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(origin, direction * hit.distance, Color.yellow);
+
+                projectionMarker.position = hit.point;
+                projectionMarker.rotation = Quaternion.FromToRotation(Vector3.forward, hit.normal);
+            }
+            else
+            {
+                Debug.DrawRay(origin, direction * 10, Color.white);
+            }
+        }
+    }
+}
---
+++ Assets/Plugins/Pupil/Scripts/RequestController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PupilLabs
+{
+
+    public partial class RequestController : MonoBehaviour
+    {
+
+        [SerializeField]
+        private Request request = new Request();
+
+        public bool IsConnected
+        {
+            get { return request.IsConnected; }
+        }
+
+        public delegate void ConnectionDelegate();
+        public event ConnectionDelegate OnConnected;
+        public event ConnectionDelegate OnDisconnecting;
+
+        // [SerializeField]
+        private string PupilVersion;
+
+        public string GetConnectionString()
+        {
+            return request.GetConnectionString();
+        }
+
+        void OnEnable()
+        {
+            PupilVersion = "not connected";
+            if (!IsConnected)
+            {
+                RunConnect();
+            }
+        }
+
+        void OnDisable()
+        {
+            if (IsConnected)
+            {
+                Disconnect();
+            }
+        }
+
+        public void RunConnect()
+        {
+            StartCoroutine(Connect(retry: true, retryDelay: 5f));
+        }
+
+        private IEnumerator Connect(bool retry = false, float retryDelay = 5f)
+        {
+            yield return new WaitForSeconds(3f);
+
+            while (!IsConnected)
+            {
+                request.InitializeRequestSocket();
+
+                if (!IsConnected)
+                {
+                    if (retry)
+                    {
+                        Debug.Log("Could not connect, Re-trying in 5 seconds ! ");
+                        yield return new WaitForSeconds(retryDelay);
+
+                    }
+                    else
+                    {
+                        request.TerminateContext();
+                        yield return null;
+                    }
+                }
+            }
+            Debug.Log(" Succesfully connected to Pupil! ");
+            UpdatePupilVersion();
+
+            // RepaintGUI(); //
+            if (OnConnected != null)
+                OnConnected();
+            yield break;
+        }
+
+        public void Disconnect()
+        {
+            if (OnDisconnecting != null)
+            {
+                OnDisconnecting();
+            }
+
+            request.CloseSockets();
+        }
+
+        public bool Send(Dictionary<string, object> dictionary)
+        {
+            return request.SendRequestMessage(dictionary);
+        }
+
+        public void StartPlugin(string name, Dictionary<string, object> args = null)
+        {
+            Dictionary<string, object> startPluginDic = new Dictionary<string, object> {
+                { "subject", "start_plugin" },
+                { "name", name }
+            };
+
+            if (args != null)
+            {
+                startPluginDic["args"] = args;
+            }
+
+            request.SendRequestMessage(startPluginDic);
+        }
+
+        public void StopPlugin(string name)
+        {
+            request.SendRequestMessage(new Dictionary<string, object> {
+                { "subject","stop_plugin" },
+                { "name", name }
+            });
+        }
+
+        public void SetPupilTimestamp(float time)
+        {
+            string response;
+            string command = "T " + time.ToString("0.000000", System.Globalization.CultureInfo.InvariantCulture);
+            Debug.Log($"Sync Time Command: {command}");
+            request.SendCommand(command, out response);
+        }
+
+        public string GetPupilVersion()
+        {
+            string pupilVersion = null;
+            request.SendCommand("v", out pupilVersion);
+            return pupilVersion;
+        }
+
+        private void UpdatePupilVersion()
+        {
+            PupilVersion = GetPupilVersion();
+            Debug.Log($"Pupil Version: {PupilVersion}");
+        }
+
+        [ContextMenu("Reset To Default Connection")]
+        public void ResetDefaultLocalConnection()
+        {
+            request.resetDefaultLocalConnection();
+        }
+    }
+}
